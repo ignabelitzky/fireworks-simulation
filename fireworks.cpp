843451bd8928e85c0c62c15d2e3d1d50807f74d1
Ignacio <ignabelitzky@gmail.com>
2023-06-28 14:49:43 -0300

    Upload of the source codes

---
+++ fireworks.cpp
@@ -0,0 +1,76 @@
+#include <SFML/Graphics.hpp>
+#include <random>
+#include <cmath>
+#include "params.h"
+
+struct Particle {
+    sf::Vector2f originalPos;
+    sf::Vector2f position;
+    sf::Vector2f velocity;
+    sf::Color color;
+    float lifespan;
+    bool mustDelete;
+};
+
+int main() {
+    constexpr int windowWidth = 1024;
+    constexpr int windowHeight = 760;
+
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Firworks Simulation");
+
+    std::vector<Particle> particles;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> velDist(100.0f, 300.0f);
+    std::uniform_real_distribution<float> angleDist(0.0f, 2 * 3.141592f);
+    std::uniform_int_distribution<int> colorDist(0, 255);
+
+    while(window.isOpen()) {
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+
+        // Create new fireworks
+        sf::Color c = sf::Color(colorDist(gen), colorDist(gen), colorDist(gen));
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            for(int i = 0; i < PARTICLE_MULTIPLIER; ++i) {
+                Particle particle;
+                particle.originalPos = sf::Vector2f(sf::Mouse::getPosition(window));
+                particle.position = particle.originalPos;
+                particle.velocity = sf::Vector2f(velDist(gen) * std::cos(angleDist(gen)), velDist(gen) * std::sin(angleDist(gen)));
+                particle.color = sf::Color(colorDist(gen), colorDist(gen), colorDist(gen));
+                particle.lifespan = 10.0f;
+                particle.mustDelete = false;
+
+                particles.push_back(particle);
+            }
+        }
+
+        // Update and draw particles
+        for(auto it = particles.begin(); it != particles.end();) {
+            it->position += it->velocity * 0.0005f;
+            it->velocity.y += 50.0f * 0.001f; // Apply gravity
+            
+            sf::CircleShape shape(PARTICLE_SIZE);
+            shape.setPosition(it->position);
+            shape.setFillColor(it->color);
+            window.draw(shape);
+
+            it->lifespan -= 0.01f;
+            float dist = std::sqrt(std::pow(it->originalPos.x - it->position.x, 2) + std::pow(it->originalPos.y - it->position.y, 2));
+            if(dist >= MAX_RADIUS) {
+                it->mustDelete = true;
+            }
+            if(it->lifespan <= 0 || it->mustDelete)
+                it = particles.erase(it);
+            else
+                ++it;
+        }
+        window.display();
+    }
+    return 0;
+}
---
+++ params.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define PARTICLE_SIZE 1.2f
+
+#define MAX_RADIUS 150.0f
+
+#define PARTICLE_MULTIPLIER 3
